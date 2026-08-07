1276099c2d716360124eeb7507ca43385751fc7f
[dyad] <tiptab@proton.me>
2026-08-07 10:41:15 +0930

    [dyad] Changed default app theme to Light Mode - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <SessionProvider>
           <TooltipProvider>
             <Toaster />
